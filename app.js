cf02667ba26aeaa59d083f1aa08fa8c1fa816e25
Alina Tarnakova <alina777pskov@mail.ru>
2023-06-20 20:10:57 +0300

    finish

---
+++ app.js
@@ -0,0 +1,95 @@
+// require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
+const limiter = require('./middlewares/limiter');
+
+const NotFoundError = require('./errors/NotFoundError');
+
+const {
+  createUser,
+  login,
+} = require('./controllers/users');
+
+const auth = require('./middlewares/auth');
+
+const router = require('./routes/index');
+
+/* const {
+  PORT,
+  BD,
+} = require('./config'); */
+
+const {
+  PORT = 3000,
+  BD = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+} = process.env;
+
+mongoose.connect(BD)
+  .then(() => {
+    console.log('БД подключена');
+  })
+  .catch(() => {
+    console.log('Не удалось подключиться к БД');
+  });
+
+const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(requestLogger);
+
+app.use(cors);
+
+app.use(limiter);
+
+// роуты, не требующие авторизации
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+// авторизация
+app.use(auth);
+
+app.use(router);
+
+app.use('/*', (req, res, next) => next(new NotFoundError('Страница не найдена.')));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
+
+app.listen(PORT, () => console.log(`Web app available at http://127.0.0.1:${PORT}`));
---
+++ controllers/movies.js
@@ -0,0 +1,97 @@
+const Movie = require('../models/movie');
+
+const BadRequestError = require('../errors/BadRequestError');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
+const NotFoundError = require('../errors/NotFoundError');
+// const ConflictError = require('../errors/ConflictError');
+const ForbiddenError = require('../errors/ForbiddenError');
+
+// возвращает все сохранённые текущим  пользователем фильмы
+// GET /movies
+module.exports.getMovie = (req, res, next) => {
+  Movie.find({})
+    .then((movie) => res.send(movie))
+    .catch(next);
+};
+
+// создаёт фильм с переданными в теле
+// country, director, duration, year, description, image,
+// trailerLink, nameRU, nameEN, thumbnail, movieId
+// POST /movies
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  const { id } = req.user;
+
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: id,
+  })
+    .then((movie) => {
+      res.send(movie);
+    })
+    .catch((err) => {
+      if (
+        res.status(err.name === 'CastError' || err.name === 'ValidationError')
+      ) {
+        next(new BadRequestError(
+          `Переданы некорректные данные при создании фильма ${err.name}.`,
+        ));
+      } else {
+        next(err);
+      }
+    });
+};
+
+// удаляет сохранённый фильм по id
+// DELETE /movies/movieId
+module.exports.deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+  const { id } = req.user;
+
+  Movie.findById(movieId)
+    .orFail()
+    .then((movie) => {
+      const { owner: movieOwnerId } = movie;
+      if (movieOwnerId.valueOf() !== id) {
+        throw new ForbiddenError('Вы не можете удалить этот фильм.');
+      } return Movie.findByIdAndDelete(movieId);
+    })
+    .then((deletedMovie) => {
+      res.send(deletedMovie);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError(
+          'Переданы некорректные данные фильма.',
+        ));
+      } else if (err.name === 'DocumentNotFoundError') {
+        next(new NotFoundError(
+          'Передан несуществующий _id фильма.',
+        ));
+      } else {
+        next(err);
+      }
+    });
+};
